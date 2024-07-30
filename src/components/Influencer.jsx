c2b9fc8d94e4d6d17fe2550e393e9df809e59b81
shktabish <shktab1sh93@gmail.com>
2024-07-30 23:13:29 +0530

    commit

--- src/components/Influencer.jsx
+++ src/components/Influencer.jsx
@@ -8,10 +8,9 @@ const Influencer = () => {
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-4 max-sm:w-10 w-20 mt-3" >
                     <line x1="0" y1="0" x2="150" y2="0" className="stroke-accent stroke-[6px]"/>
                 </svg>  
-                <div className="max-sm:text-xl max-md:text-4xl max-lg:text-2xl text-4xl font-bold" >One-Stop Solution for</div>
-                <div className="max-sm:text-2xl max-md:text-5xl max-lg:text-3xl text-[2.65rem] font-bold text-accent" >Influencer Marketing</div>
-                <p className="max-sm:text-base max-md:text-lg max-lg:text-base text-lg font-medium" >Gone are the days of juggling multiple tools and platforms. Our AI-powered influencer search tool is an all-in-one solution, providing end-to-end support for your influencer marketing campaigns.</p>
-                <p className="max-sm:text-base max-md:text-lg max-lg:text-base text-lg font-medium" >From discovering influencers to connecting, managing campaigns, and analyzing results, our platform streamlines the entire process. Boost your brand's visibility, reach, and credibility in the web 3 space with ease</p>
+                <div className="max-sm:text-2xl max-md:text-5xl max-lg:text-3xl text-[2.65rem] font-bold text-accent" >Enhance your KOL round with precision targeting</div>
+                <p className="max-sm:text-base max-md:text-lg max-lg:text-base text-lg font-medium" >
+                Discover and engage the perfect Key Opinion Leaders (KOLs) for your token launch, meticulously selected based on their niche expertise, geographic relevance, and expansive content reach. Seamlessly communicate, negotiate, and collaborate with top-tier influencers to effectively execute and manage your campaigns.</p>
             </div>
             <img src="./whale.svg" alt="whale" className="absolute max-sm:-top-10 -top-24 max-sm:right-4 right-10 w-36 max-sm:w-16" />
             <img src="./rocket.svg" alt="rocket" className="absolute bottom-0 right-0 max-sm:w-24 w-32" />
--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { pagesData } from '../constants/data';
 
 const Discover = () => {
   const { title } = useParams();
   const pageData = pagesData.find(page => page.title === title.replace(/-/g, ' '));
+  const [isLoading, setIsLoading] = useState(true);
 
   if (!pageData) {
     return <div>Page not found</div>;
@@ -14,11 +15,24 @@ const Discover = () => {
     <div className="min-h-screen">
       <div className="max-sm:mx-6 max-md:mx-16 mx-28 pb-20">
         <div>Vitamin I {' > '} {pageData.heading}</div>
-        <div className="max-sm:text-2xl max-lg:text-3xl text-5xl font-bold text-accent pb-10 pt-5">{pageData.heading}</div>
+        <div className="max-sm:text-2xl max-lg:text-3xl text-5xl font-bold text-accent pb-10 pt-5">
+          {pageData.heading}
+        </div>
         <div>
-          <img src="./pages/discover.svg" alt="discover image" className='float-start max-md:pr-5 pr-10 max-md:pb-5 pb-10 max-sm:w-3/5 max-lg:w-2/4 w-2/5' />
+          <div 
+            className={`float-start ${isLoading ? 'bg-white/10 animate-pulse' : ''} max-md:mr-5 mr-10 max-md:mb-5 mb-10 max-sm:w-3/5 max-lg:w-2/4 w-2/5`} 
+          >
+            <img 
+              src="./pages/discover.png" 
+              alt="discover image" 
+              className='float-start' 
+              width="500" 
+              height="300" 
+              onLoad={() => setIsLoading(false)} 
+            />
+          </div>
           {pageData.text.map((paragraph, index) => (
-            <p key={index} className="mt-4">
+            <p key={index} className="mt-4 text-lg">
               {paragraph}
             </p>
           ))}
